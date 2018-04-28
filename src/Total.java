8b7bbf666d341d51243fcfd8c00548b816e82f79
tianzhefang <tianzhefang@sinosoft.com.cn>
2018-04-28 15:16:29 +0800

    删掉几个无用的文件

--- src/Total.java
+++
@@ -1,9 +0,0 @@
-//import com.sinosoft.lis.pubfun.PubFun;
-
-
-public class Total {
-
-
-    //preStatDate本次提数的计算起期的前一天
-//    String preStatDate = PubFun.calDate(mLAWageLogSchema.getStartDate(), -1, "D",null);
-}
